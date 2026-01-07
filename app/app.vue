8d46e84b468fe7aad9cab428209362072e9cc51f
Ramiris <sirchopwood@gmail.com>
2026-01-07 23:25:22 +0000

    Add Tailwind, Projects and Layout

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,11 @@
+<script setup lang="ts">
+</script>
 <template>
-  <NuxtRouteAnnouncer />
-  <NuxtPage />
+  <div class="bg-stone-700 fixed left-0 top-0 w-screen h-screen -z-50"/>
+  <div class="text-white">
+    <NuxtLayout>
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
